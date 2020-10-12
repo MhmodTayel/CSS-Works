fea388ddc207872dce7cd78845589ed4fba0fd1d
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-12 23:03:44 +0200

    Sleek checkbox User Interface Design

---
+++ Sleek checkbox User Interface Design/main.js
@@ -0,0 +1,4 @@
+document.querySelector(".toggle").addEventListener("click", function () {
+  document.querySelector(".toggle").classList.toggle("toggleOn");
+  document.querySelector(".inner").classList.toggle("innerOn");
+});
