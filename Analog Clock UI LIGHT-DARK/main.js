5bbf6406fb41585171e7fe0112a3aee053b9ebea
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-18 23:10:29 +0200

    dark clock

---
+++ Analog Clock UI LIGHT-DARK/main.js
@@ -0,0 +1,15 @@
+const deg = 6;
+const hr = document.querySelector("#hr");
+const min = document.querySelector("#min");
+const sec = document.querySelector("#sec");
+
+setInterval(() => {
+  let day = new Date();
+  let hour = day.getHours() * 30;
+  let minute = day.getMinutes() * deg;
+  let second = day.getSeconds() * deg;
+
+  hr.style.transform = `rotateZ(${hour + minute / 12}deg)`;
+  min.style.transform = `rotateZ(${minute}deg)`;
+  sec.style.transform = `rotateZ(${second}deg)`;
+});
