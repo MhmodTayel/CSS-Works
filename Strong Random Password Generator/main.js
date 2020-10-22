044567d7dcd4eb0b1f605ec80e16631c7ff1a72a
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-22 18:51:56 +0200

    Strong Random Password Generator

--- Strong Random Password Generator/main.js
+++ Strong Random Password Generator/main.js
@@ -1,13 +1,20 @@
 function getPassword() {
-  var chars = '0123456789zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP`@!”?$?%^&*()_–+={[}]:;‘~#|<,>.?/'
+  var chars =
+    "0123456789zxcvbnmasdfghjklqwertyuiopZXCVBNMASDFGHJKLQWERTYUIOP`@!”?$?%^&*()_–+={[}]:;‘~#|<,>.?/";
 
-  var passwordLength = 16;
-  var password = '';
-
-  for(var i = 0; i < passwordLength; i++) {
-    var random = Math.floor(Math.random()* chars.length);
-    password += chars.slice(random,random+1);
+  var password = "";
+  var passwordLength = document.getElementById("range").value;
+  for (var i = 0; i < passwordLength; i++) {
+    var random = Math.floor(Math.random() * (chars.length-3));
+    password += chars.slice(random, random + 1);
   }
-  document.getElementById('password').value= password;
+  document.getElementById("password").value = password;
+}
+
+function change() {
+
+    var passwordLength = document.getElementById("range").value;
+    document.querySelector('.Value').textContent=passwordLength;
+  
 }
 
