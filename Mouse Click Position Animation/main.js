b567f0068b3780b220f260e7ee987728538a205e
Mahmoud Tayel <mhmodtayel0050@gmail.com>
2020-10-30 16:58:34 +0200

    Mouse Click Position Animation

--- Mouse Click Position Animation/main.js
+++ Mouse Click Position Animation/main.js
@@ -1,5 +1,11 @@
 document.querySelector(".btn").addEventListener("click", onClick);
-
+let spanBG = document.querySelector("span");
 function onClick(e) {
-  console.log(e);
+  console.log(e.offsetY, e.offsetX);
+  spanBG.style.top = `${e.offsetY}px`;
+  spanBG.style.left = `${e.offsetX}px`;
+  setTimeout(() => {
+    spanBG.style.transform = `translate(-50%,-50%)scale(1)`;
+  },500)
+  spanBG.style.transform = `translate(-50%,-50%)scale(0)`;
 }
